9e5730ea1cdc579ff906de631aa18521721c0381
Amos Wenger <ndd@rylliog.cz>
2011-03-22 15:39:25 +0100

    Finished tests for uses, including fixtures

--- test/users_test.rb
+++ test/users_test.rb
@@ -28,11 +28,44 @@ class UsersTest < Test::Unit::TestCase
       user.country_id.should == 'CH'
     end
     
-    should "retrieve the first two tracks of chad" do
+    should "retrieve the first two tracks of chab" do
       stub_get('http://api.official.fm/user/chab/tracks?key=GNXbH3zYb25F1I7KVEEN&format=json&api_embed_codes=&api_max_responses=2', 'user_tracks.json')
       tracks = @client.user_tracks('chab', {:limit => 2})
       tracks.size.should == 2
-      tracks[0].name.should != nil
+      tracks[0].title.should == 'Blind (Chab Remix)'
+      tracks[0].artist_string.should == 'Hercules and Love Affair'
+    end
+    
+    should "retrieve the first two playlists of chab" do
+      stub_get('http://api.official.fm/user/chab/playlists?key=GNXbH3zYb25F1I7KVEEN&format=json&api_embed_codes=&api_max_responses=2', 'user_playlists.json')
+      playlists = @client.user_playlists('chab', {:limit => 2})
+      playlists.size.should == 2
+      playlists[0].name.should == 'I like'
+      playlists[1].user_id.should == 8735 # chab's user id
+    end
+    
+    should "retrieve the first two contacts of chab" do
+      stub_get('http://api.official.fm/user/chab/contacts?key=GNXbH3zYb25F1I7KVEEN&format=json&api_max_responses=2', 'user_contacts.json')
+      contacts = @client.user_contacts('chab', {:limit => 2})
+      contacts.size.should == 2
+      contacts[0].name.should == 'Mental Groove Records'
+      contacts[1].status.should == 'artist'
+    end
+    
+    should "retrieve the first two subscribers of chab" do
+      stub_get('http://api.official.fm/user/chab/subscribers?key=GNXbH3zYb25F1I7KVEEN&format=json&api_max_responses=2', 'user_subscribers.json')
+      subscribers = @client.user_subscribers('chab', {:limit => 2})
+      subscribers.size.should == 2
+      subscribers[0].id.should == 394
+      subscribers[1].name.should == 'jenny'
+    end
+    
+    should "retrieve the first two subscriptions of chab" do
+      stub_get('http://api.official.fm/user/chab/subscriptions?key=GNXbH3zYb25F1I7KVEEN&format=json&api_max_responses=2', 'user_subscriptions.json')
+      subscriptions = @client.user_subscriptions('chab', {:limit => 2})
+      subscriptions.size.should == 2
+      subscriptions[0].profile.should == 'Freelance CTO, Junior tastemaker!'
+      subscriptions[1].url.should == 'http://www.myspace.com/sumomusic'
     end
 
   end
